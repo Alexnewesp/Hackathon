76ddc807b8005222091c36b665c888ddbc50966e
Usrg30 <alexusrg30gmail.com>
2022-04-06 11:58:17 +0200

    Hackathon Getafe 2022

---
+++ app/src/main/java/com/example/mercadillosmadridmapa/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mercadillosmadridmapa;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void abrirlistamercadillo(View view) {
+        Intent i = new Intent(this,MapsActivity.class);
+        startActivity(i);
+    }
+
+
+}
